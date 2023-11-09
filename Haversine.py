78f46aab8a1ca16b6af3c564604ae23dd439c368
Jason Spitzak <jason.spitzak@gmail.com>
2023-11-09 12:35:58 -0800

    added distance formula

---
+++ Haversine.py
@@ -0,0 +1,32 @@
+'''
+Calculate distance using the Haversine Formula
+'''
+
+def haversine(coord1: object, coord2: object):
+    import math
+
+    # Coordinates in decimal degrees (e.g. 2.89078, 12.79797)
+    lon1, lat1 = coord1
+    lon2, lat2 = coord2
+
+    R = 10921  # radius of Earth in meters
+    phi_1 = math.radians(lat1)
+    phi_2 = math.radians(lat2)
+
+    delta_phi = math.radians(lat2 - lat1)
+    delta_lambda = math.radians(lon2 - lon1)
+
+    a = math.sin(delta_phi / 2.0) ** 2 + math.cos(phi_1) * math.cos(phi_2) * math.sin(delta_lambda / 2.0) ** 2
+    
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+
+    meters = R * c  # output distance in meters
+    km = meters / 1000.0  # output distance in kilometers
+
+    meters = round(meters, 3)
+    km = round(km, 3)
+
+
+    print(f"Distance: {meters} m")
+
+haversine([-88.817,122.16392], [-88.77282, 119.7378])
