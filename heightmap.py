07782bf3bbed24f4b2eba6da2b34908cc32af26c
CalebNeal07 <caleboneal07@gmail.com>
2023-12-06 17:27:35 -0500

    Almost all visualization modes working

--- heightmap.py
+++ heightmap.py
@@ -2,9 +2,9 @@ import numpy as np
 import pandas as pd
 from PIL import Image
 import math
+import json
 
-def normalize(num):
-    return 255 * ((num - z.min()) / (z.max() - z.min()))
+data = {}
 
 height = np.array(pd.read_csv("data/FY23_ADC_Height_PeakNearShackleton.csv"))
 latitude = np.array(pd.read_csv("data/FY23_ADC_Latitude_PeakNearShackleton.csv"))
@@ -13,37 +13,64 @@ slope = np.array(pd.read_csv("data/FY23_ADC_Slope_PeakNearShackleton.csv"))
 
 dimensions = height.shape
 
-print(dimensions)
+data["data_x"] = dimensions[0]
+data["data_y"] = dimensions[1]
 
 height = height.flatten()
 latitude = latitude.flatten()
 longitude = longitude.flatten()
-slope = slope.flatten()
 
-x = list(map(lambda n: (n[1] + 1737400) * math.cos(math.radians(latitude[n[0]])) * math.cos(
-    math.radians(longitude[n[0]])), enumerate(height)))
-y = list(map(lambda n: (n[1] + 1737400) * math.cos(math.radians(latitude[n[0]])) * math.sin(
-    math.radians(longitude[n[0]])), enumerate(height)))
-z = np.array(list(map(lambda n: (n[1] + 1737400) * math.sin(math.radians(latitude[n[0]])), enumerate(height))))
+# Convert to cartesian coordinates
+x = list(
+    map(
+        lambda n: (n[1] + 1737400)
+        * math.cos(math.radians(latitude[n[0]]))
+        * math.cos(math.radians(longitude[n[0]])),
+        enumerate(height),
+    )
+)
+y = list(
+    map(
+        lambda n: (n[1] + 1737400)
+        * math.cos(math.radians(latitude[n[0]]))
+        * math.sin(math.radians(longitude[n[0]])),
+        enumerate(height),
+    )
+)
+z = np.array(
+    list(
+        map(
+            lambda n: (n[1] + 1737400) * math.sin(math.radians(latitude[n[0]])),
+            enumerate(height),
+        )
+    )
+)
 
 print(min(x), max(x), max(x) - min(x))
 print(min(y), max(y), max(y) - min(y))
 print(z.min(), z.max())
 
+# Normalize Z data
+z *= -1
 z -= z.min()
 z /= z.max() - z.min()
 z *= 255
 z = np.uint8(z)
 z = z.reshape(dimensions)
 
-print(z)
-
-print("Generating image")
-
 heightmap = Image.fromarray(z)
+slope = Image.fromarray(slope)
 
-heightmap = heightmap.resize((16385, 16385), resample=Image.Resampling.BICUBIC)
+elevation = heightmap.resize((16385, 16385), resample=Image.Resampling.BICUBIC)
+heightmap.thumbnail((511, 511), Image.Resampling.LANCZOS)
 
+slope.verify
 heightmap.verify()
+elevation.verify()
 
-heightmap.save('./heightmap.png')
+slope.save("./textures/slope.png")
+elevation.save("./textures/elevation.png")
+heightmap.save("./textures/heightmap.png")
+
+with open("data.json", "w") as file:
+    json.dump(data, file)
--- main.py
+++ main.py
@@ -1,31 +1,64 @@
 from ursina import *
-from ursina.shaders import lit_with_shadows_shader
+from enum import Enum
+
+global terrain 
+
+class Mode(Enum):
+    HEIGHT = './textures/height.png'
+    SLOPE = './textures/slope.png'
+    ELEVATION = './textures/elevation.png'
+    AZIMUTH = './textures/azimuth.png'
+
+mode = Mode.ELEVATION
+
+def change_mode():
+    if mode == Mode.HEIGHT:
+        mode = Mode.SLOPE
+    elif mode == Mode.SLOPE:
+        mode = Mode.ELEVATION
+    elif mode == Mode.ELEVATION:
+        mode = Mode.AZIMUTH
+    else:
+        mode = Mode.HEIGHT
+    
+    terrain.texture = mode.value
+
 
 def main():
-    app = Ursina()
+    app = Ursina(
+        title="NASA - ADC",
+        icon='./textures/Artemis_program.png',
+        borderless=False,
+        fullscreen=False,
+        development_mode=False
+    )
 
-    moon = Entity(
-        model=Terrain("hm"),
-        shader=lit_with_shadows_shader,
-        scale=(40,5,20))
+    window.color = color.black
     
-    hv = moon.model.height_values.tolist()
-    moon_terrain = Entity(model=Terrain(height_values=hv), scale=(40,5,20), texture='hm', x=40)
+    terrain = Entity(model=Terrain("./textures/heightmap.png"), texture="./textures/elevation.png")
 
-    moon.color = color.gray  # Color of the mesh
+    view_mode_button = Button(
+        #model='quad',
+        scale=1,
+        text="Mode",
+        origin=(-12, 9)
+    )
+    view_mode_button.on_click = change_mode
 
-    print("Setting up lighting")
-
-    # Create a light source
-    #light = AmbientLight(color = color.rgba(100, 100, 100, 0.1))
-    light = DirectionalLight(parent=moon, shadows=True)
-    light.rotation = (45, 45, 0)
-    light.color = color.white
+    quit_button = Button(
+        model='quad', 
+        scale=0.05, 
+        color=color.dark_gray, 
+        text="Quit", text_size=0.5, 
+        text_color=color.white,
+        origin=(12, 9)
+    )
+    quit_button.on_click = exit
 
     EditorCamera()
 
     app.run()
-
+    
 
 if __name__ == "__main__":
     main()
